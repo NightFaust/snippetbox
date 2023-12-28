7a60828d5649296d4d105e8f93297b627b417517
lseguin <lseguin@activeassurances.fr>
2023-12-28 09:01:58 +0100

    moved neutered file system in routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -34,30 +33,4 @@ type config struct {
 
 type application struct {
 	logger *slog.Logger
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, _ := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"path/filepath"
+)
 
 func (app *application) routes(staticDir string) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir(staticDir)})
+
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -14,4 +18,30 @@ func (app *application) routes(staticDir string) *http.ServeMux {
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	return mux
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, _ := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
