1788ac66804d6b18f1eb1ab3ade25c745c4ff532
lseguin <lseguin@activeassurances.fr>
2024-01-04 00:26:03 +0100

    request context for authentication and authorization

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -77,7 +77,11 @@ func (app *application) decodePostForm(r *http.Request, target any) error {
 
 // isAuthenticated return true if the current request is from an authenticated user
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+	return isAuthenticated
 }
 
 // NewTemplateData return a new templateData struct
